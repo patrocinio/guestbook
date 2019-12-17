c38e3e72b55856b2f50580e2861a5c9140f63c26
Eduardo Patrocinio <epatro@gmail.com>
2019-12-17 13:52:15 -0500

    Switch to IBM MQ

--- backend/queue.js
+++ backend/queue.js
@@ -1,29 +1,8 @@
-var mq = require('ibmmq');
-
-// const queueHost = process.env.QUEUE_HOST || "icp-proxy.patrocinio8-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud:31202";
-
-const qMgr = "QM1";
-
-const cno = new mq.MQCNO();
-
-cno.Options = mq.MQC.MQCNO_CLIENT_BINDING;
-
-const cd = new mq.MQCD();
-cd.ConnectionName = "icp-proxy.patrocinio8-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud(31202)";
-cd.ChannelName = "SYSTEM.DEF.SVRCONN";
-cno.ClientConn = cd;
+var amqp = require('amqplib/callback_api');
+const queueHost = process.env.QUEUE_HOST || "my-mq-v4-ibm-mq.mq.svc:1414"
 
 function createMQConnection(queue_name, callback) {
-    console.log ("Connecting to MQ at " + cd.ConnectionName);
-
-    mq.Connx(qMgr, cno, function (err, conn) {
-      if (err) {
-        console.log (formatError(err))
-      } else {
-        console.log ("MQCONN to %s successful", qMgr);
-      }
-    })
-    /*
+    console.log ("Connecting to MQ at " + queueHost)
     amqp.connect('amqp://' + queueHost, function(err, conn) {
         if (err) {
             console.log ("Error: " + err)
@@ -36,7 +15,7 @@ function createMQConnection(queue_name, callback) {
             });
         }
     });
-*/
+
 }
 
 module.exports = {
