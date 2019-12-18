927333fc8a9d218e07a7921be09b43ab3cb899c5
Eduardo Patrocinio <epatro@gmail.com>
2019-12-18 10:23:46 -0500

    Display error

--- backend/queue.js
+++ backend/queue.js
@@ -1,10 +1,8 @@
 var mq = require('ibmmq');
 
-// const queueHost = process.env.QUEUE_HOST || "icp-proxy.patrocinio8-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud:31202";
-
 const qMgr = "QM1";
 
-const cno = new mq.MQCNO(); 
+const cno = new mq.MQCNO();
 
 cno.Options = mq.MQC.MQCNO_CLIENT_BINDING;
 
@@ -18,25 +16,11 @@ function createMQConnection(queue_name, callback) {
 
     mq.Connx(qMgr, cno, function (err, conn) {
       if (err) {
-        console.log (formatError(err))
+        console.log (err)
       } else {
         console.log ("MQCONN to %s successful", qMgr);
       }
     })
-    /*
-    amqp.connect('amqp://' + queueHost, function(err, conn) {
-        if (err) {
-            console.log ("Error: " + err)
-        } else {
-            console.log ("Creating queue " + queue_name)
-            conn.createChannel(function(err, ch) {
-                ch.assertQueue(queue_name, {durable: false});
-
-                callback(ch, queue_name)
-            });
-        }
-    });
-*/
 }
 
 module.exports = {
