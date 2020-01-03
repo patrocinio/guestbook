4bcbc22909aad0e84210ee32c2df49d3170796ab
Eduardo Patrocinio <epatro@gmail.com>
2020-01-03 10:22:45 -0500

    Use MCM application

--- deploy_channel.sh
+++ deploy_channel.sh
@@ -1,4 +1,5 @@
 cd helm
+oc new-project guestbook-channel
 helm package channel
 helm delete --purge channel --tls
 helm install channel -n channel --namespace guestbook --tls -f guestbook/values-openshift.yaml
--- deploy_mcm_application.sh
+++ deploy_mcm_application.sh
@@ -1,4 +1,7 @@
 cd helm
+oc new-project guestbook-application
+oc project guestbook-application
+
 helm package application
 helm delete --purge application --tls
-helm install application -n application --namespace guestbook --tls 
+helm install application -n application --namespace guestbook-application --tls
