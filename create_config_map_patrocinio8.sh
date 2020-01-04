05fd011b68d96beaa81fd797ee5074ea957fc7d1
Eduardo Patrocinio <epatro@gmail.com>
2020-01-04 14:37:28 -0500

    Create ConfigMap in patrocinio8 cluster

---
+++ create_config_map_patrocinio8.sh
@@ -0,0 +1,2 @@
+oc project guestbook-application
+oc create configmap guestbook-config --from-literal=backend-url=backend-guestbook.patrocinio9-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud
