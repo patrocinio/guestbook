b6685dfc1ebee95f54495d0f3da73ad41da4c1f4
Eduardo Patrocinio <epatro@gmail.com>
2020-07-16 09:58:49 -0400

    Deploy using Helm

--- delete_resources.sh
+++ delete_resources.sh
@@ -7,6 +7,12 @@ oc delete svc redis-slave
 oc delete deploy redis-master
 oc delete deploy redis-slave
 oc delete deploy backend
+oc delete deploy consumer
+oc delete deploy frontend
+oc delete deploy messaging
 
 oc delete pvc redis-master-pvc
 oc delete pvc redis-slave-pvc
+
+oc delete route backend
+oc delete route frontend
