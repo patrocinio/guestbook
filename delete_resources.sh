abd5cae1df8944d8ff3b0710cecf2e8696719a27
Eduardo Patrocinio <epatro@gmail.com>
2020-07-16 09:37:50 -0400

    Work on Helm branch

--- delete_resources.sh
+++ delete_resources.sh
@@ -1,3 +1,12 @@
 oc delete svc backend
+oc delete svc frontend
+oc delete svc messaging
+oc delete svc redis-master
+oc delete svc redis-slave
+
 oc delete deploy redis-master
+oc delete deploy redis-slave
+oc delete deploy backend
+
 oc delete pvc redis-master-pvc
+oc delete pvc redis-slave-pvc
