9313ab72d64edcef7b54cb03bbc5fe408f983ba3
Eduardo Patrocinio <epatro@gmail.com>
2020-07-28 19:28:05 -0400

    Create Prevail version

--- deploy_helm_openshift.sh
+++ deploy_helm_openshift.sh
@@ -1,6 +1,13 @@
+PROJECT=guestbook-eduardo
+
 cd helm
+
+oc new-project $PROJECT
+
+oc project $PROJECT
+
 helm package guestbook
 
 helm delete guestbook
 
-helm install guestbook guestbook --namespace guestbook -f guestbook/values-openshift.yaml
+helm install guestbook guestbook --namespace $PROJECT -f guestbook/values-openshift.yaml
