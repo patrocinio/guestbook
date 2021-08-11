3de525a98eda4a24fc0e906a853ca3f58f0e306f
Eduardo Patrocinio <epatro@gmail.com>
2021-08-11 17:19:00 -0400

    Port to latest version

--- php-redis/controllers.js
+++ php-redis/controllers.js
@@ -1,32 +1,45 @@
+/**
+ * Copyright 2016 The Kubernetes Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var redisApp = angular.module('redis', ['ui.bootstrap']);
 
+/**
+ * Constructor
+ */
 function RedisController() {}
 
 RedisController.prototype.onRedis = function() {
-  console.log ("append");
-
-    this.http_.get("guestbook.php?cmd=append&key=messages&value=" + this.scope_.msg)
+    this.scope_.messages.push(this.scope_.msg);
+    this.scope_.msg = "";
+    var value = this.scope_.messages.join();
+    this.http_.get("guestbook.php?cmd=set&value=" + value)
             .success(angular.bind(this, function(data) {
-              console.log(data);
-              this.scope_.messages = data.data.split(",");
+                this.scope_.redisResponse = "Updated.";
             }));
 };
 
 redisApp.controller('RedisCtrl', function ($scope, $http, $location) {
-    console.log ("Initializing");
-
         $scope.controller = new RedisController();
         $scope.controller.scope_ = $scope;
         $scope.controller.location_ = $location;
         $scope.controller.http_ = $http;
 
-        $scope.controller.http_.get("guestbook.php?cmd=get&key=messages")
+        $scope.controller.http_.get("guestbook.php?cmd=get")
             .success(function(data) {
                 console.log(data);
-                if (data.data) { 
-                  $scope.messages = data.data.split(",");
-                } else {
-                  $scope.messages = "";
-                }
+                $scope.messages = data.data.split(",");
             });
 });
